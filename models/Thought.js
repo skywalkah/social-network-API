331b182eabd39dcc69fe33c7910fb090e1d8e6bd
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-07-19 17:59:02 -0400

    Adding models

---
+++ models/Thought.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment'); // optionally you can use another date library
+
+const ReactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
